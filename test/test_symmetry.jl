4234152a0283146a1ee54af711b9b76e5bc49918
Zhaohui Zhi <2276780832@qq.com>
2025-08-11 18:58:10 +0800

    trigger CI

--- test/test_symmetry.jl
+++ test/test_symmetry.jl
@@ -1,10 +1,5 @@
 using Test
 
-@testset "tenary" begin
-    @test tern_to_base10(dit"120120011221112020;3") == 223302561
-    @test ternary_pad(30, 12) == dit"000000001010;3"
-end
-
 @testset "neighbors" begin
     @test Set(get_neighbors(3, 3, :x)) == Set([(0, 3), (2, 5), (1, 4), (6, 9), (7, 10), (8, 11), (12, 15), (13, 16), (14, 17)])
     @test Set(get_neighbors(3, 3, :y)) == Set([(3, 6), (4, 7), (5, 8), (9, 12), (10, 13), (11, 14), (0, 15), (1, 16), (2, 17)])
