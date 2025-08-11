1861c476154cb1ee241fe2ccfddfc0abd107c8e0
Documenter.jl <documenter@juliadocs.github.io>
2025-08-11 09:50:08 +0000

    build based on eab63b4

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#HoneycombQSL","page":"Home","title":"HoneycombQSL","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HoneycombQSL.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#LatticeQSL","page":"Home","title":"LatticeQSL","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LatticeQSL.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
