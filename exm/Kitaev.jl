4ead567494bfa3f1474bd312e9d3961dcb67eb6d
Zhaohui Zhi <2276780832@qq.com>
2025-08-13 14:05:26 +0800

    add check

--- exm/Kitaev.jl
+++ exm/Kitaev.jl
@@ -18,13 +18,28 @@ function check_Wp(m::Int,n::Int)
     return loop_val_lis
 end
 
+function check_excitation(m::Int,n::Int, idx::Vector{Int})
+    vals, vecs = load("./exm/data/Kitaev_eigenm$(m)n$(n).jld", "vals","vecs")
+    @show vals
+    fllis = [loop_op(i, j, m, n) for i in 1:m, j in 1:n]
+    loop_val_mat = [vecs[:,idx]'*fllis[i,j]*vecs[:,idx] for i in 1:m, j in 1:n]
+
+    return loop_val_mat
+end
+
+# eigvals of 3*3, 2*4, 3*4
+# -14.2915  -12.9443 -19.09178532237227
+#  -14.2915  -12.9443 -18.98699905558389
+#  -14.2915  -12.9443 -18.82203651360451
+#  -14.0746  -12.9443 -18.822036513604388
+#  -14.0746  -12.7929 -18.822036513604083
+#  -14.0746  -12.7929
+#  -14.0746  -12.7929
+#  -14.0746  -12.7929
+#  -14.021   -12.5851
+#  -14.021   -12.5851
+
 loop_val_lis24 = check_Wp(2,4)
 loop_val_lis33 = check_Wp(3,3)
 
-Wp1 = loop_op(1, 1, 3, 3)
-
-# NEED to nothing that it seems the loop value is not all 1 or -1, just because of the degeneracy.
-stlis = vecs[:, 4:12]
-newWp1 = stlis'*Wp1 * stlis
-
-eigvals(newWp1)
+loop_val_mat = check_excitation(2,4, collect(5:8))
